56dbbb9ed4e8b5bcd874c40da9ba131bdb4b2d4c
Danil <dextwelwe@gmail.com>
2024-03-06 21:07:59 -0500

    main

---
+++ BE/car-comparaison/src/test/java/com/dextwelwe/carcomparaison/CarComparaisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dextwelwe.carcomparaison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarComparaisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
