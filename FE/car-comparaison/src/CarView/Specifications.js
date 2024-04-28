2fab9d69fcf45780ade275bc256a4b30389219d7
Danil <dextwelwe@gmail.com>
2024-04-28 09:57:04 -0400

    icones pour les preferences ajoute avec le layout

--- FE/car-comparaison/src/CarView/Specifications.js
+++ FE/car-comparaison/src/CarView/Specifications.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Specifications({car}) {
   return (
     <div>
-      <ul className='listINFO'>
+      <ul className='listINFO' style={{fontSize : '17px'}}>
         <li><span style={{fontWeight : "bold"}}>ENGINE</span>: {car.moteur}</li>
         <li><span style={{fontWeight : "bold"}}>FUEL TYPE : </span>{car.typeCarburant}</li>
         <li><span style={{fontWeight : "bold"}}>AUTONOMY : </span>{car.autonomie}</li>
         <li><span style={{fontWeight : "bold"}}>POWER : </span>{car.puissance}</li>
         <li><span style={{fontWeight : "bold"}}>TORQUE : </span>{car.couple}</li>
-        <li><span style={{fontWeight : "bold"}}>TRANSMISSION : {car.transmission}</span></li>
+        <li><span style={{fontWeight : "bold"}}>TRANSMISSION : </span>{car.transmission}</li>
         <li><span style={{fontWeight : "bold"}}>DRIVETRAIN : </span>{car.motricite}</li>
         <li><span style={{fontWeight : "bold"}}>TRANSMISSION : </span>{car.transmission}</li>
         <li><span style={{fontWeight : "bold"}}>VEHICULE TYPE : </span>{car.transmission}</li>
